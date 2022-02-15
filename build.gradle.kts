b646fa7dbafba0df183b6bab6ad7ea5258eafce5
naijun0403 <65700898+archethic@users.noreply.github.com>
2022-02-16 00:56:05 +0900

    change version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 group = "org.socis"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     implementation("com.google.code.gson:gson:2.9.0")
 }
